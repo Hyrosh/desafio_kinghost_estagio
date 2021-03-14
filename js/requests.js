ea6c2f491f00a6289c835fd62dba0d30b488ac31
LUCAS DI MARE <56893770+Hyrosh@users.noreply.github.com>
2021-03-14 10:24:36 -0300

    Add files via upload

---
+++ js/requests.js
@@ -0,0 +1,62 @@
+const privateKey = "8c509daf27f714ba14feb113764582980a708647";
+const publicKey = "20ea9778a1425ce9471bde5e5c8dcfc6";
+
+
+
+
+
+function createHash(timeStamp) {
+
+    const toBeHashed = timeStamp + privateKey + publicKey;
+    const hashedMessage = md5(toBeHashed);
+    return hashedMessage;
+
+}
+
+function getCharacterList() {
+
+    //tempo agora
+    const timeStamp = Date.now().toString();
+    //numero do herois
+    const offset = Math.floor(572);
+    //hash para validar a requisição
+    const hash = createHash(timeStamp);
+
+    
+    const urlAPI = "http://gateway.marvel.com/v1/public/characters?limit=9&offset="+offset+"&ts="+timeStamp+"&apikey="+publicKey+"&hash="+hash;
+
+    // sla mas funcionou
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            getImages(data);
+        }
+    };
+    xhttp.open("GET", urlAPI, true);
+    xhttp.send();
+}
+
+
+function showHistorys(elemento) {
+
+    const codigo = elemento.parentNode.getElementsByTagName("h5")[1].textContent.substring(4, 11);//codigo do personagem
+    console.log(codigo);
+    const timeStamp = Date.now().toString();//tempo agora
+    const hash = createHash(timeStamp);//hash para validar a requisição
+
+    const urlAPI = "https://gateway.marvel.com:443/v1/public/characters/"+codigo+"/stories?ts="+timeStamp+"&apikey="+publicKey+"&hash="+hash;
+    console.log(urlAPI);
+    
+    // sla mas funcionou
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            getHistorys(data);
+        }
+    };
+    xhttp.open("GET", urlAPI, true);
+    xhttp.send();
+
+}
